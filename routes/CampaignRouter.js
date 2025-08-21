94f33705eb2288edf4db9218a3aafe1ca3afe90b
root <187004982+ugbhbeh@users.noreply.github.com>
2025-08-21 04:08:02 +0000

    added new campaign route all on my own

--- routes/CampaignRouter.js
+++ routes/CampaignRouter.js
@@ -1,4 +1,4 @@
-const {Router} = require("express");
+const {Router, json} = require("express");
 const {PrismaClient} = require("@prisma/client");
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
@@ -6,11 +6,54 @@ const {authenticateToken} = require("../middleware/Auth");
 const CampaignRouter = Router();
 const prisma = new PrismaClient();
 
+// return all campaigns created by a user
+
 // create campaign 
+
+CampaignRouter.post('/new', authenticateToken, async (req, res) => {
+    const userId = req.user.userId;
+    const {name} = req.body
+try {
+     const campaign = await prisma.campaign.create({
+        data: {
+            userId,
+            name
+        },
+        select: {
+            id: true,
+            name: true,
+            createdAt: true,
+            user: {
+                select: {
+                id: true,
+                email: true,
+                name: true,
+            },
+            customers:{
+                select:{
+                id: true,
+                email: true,
+                name: true
+            }
+            },   
+        }
+      }
+     });
+     res.status(201).json(campaign);
+} catch (err) {
+    res.status(400).json({error: err.message})
+}
+});
 
 // upload csv of clients and store them in data base
 
 // route to manually add clients to database
+
+
+
+// delete clients from a campaign one by one 
+
+// delete a campaign 
 
 
 
