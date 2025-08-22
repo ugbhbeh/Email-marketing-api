661227b4610a92b9fbbef81cdda2b3142eda53e2
root <187004982+ugbhbeh@users.noreply.github.com>
2025-08-22 05:53:24 +0000

    added manual route for adding client

--- routes/CustomerRouter.js
+++ routes/CustomerRouter.js
@@ -40,9 +40,46 @@ CustomerRouter.post("/csv", authenticateToken, async (req, res) => {
   }
 });
 
-
-
 // add manual clients 
+
+CustomerRouter.post("/single", authenticateToken, async(req, res) => {
+     const userId = req.user.userId;
+     const {email, name} = req.body;
+
+     if(!email) {
+        return res.status(400).json({ error: " Customer Email is required" })
+     }
+
+     try {
+        const user = await prisma.user.findUnique({where: { id: userId }});
+        if (!user) {
+            return res.status(401).json({error: "User not found"});
+        };
+
+        const customer = await prisma.customer.upsert({
+            where: {
+                email_userId: { email, userId }, 
+               },
+            update: {
+                name: name || null,
+               },
+            create: {
+                email,
+                name: name || null,
+                userId,
+               },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+               },
+            });
+     res.status(200).json(customer);
+
+     }catch (error) {
+        res.status(400).json({ error: error.message });
+     }
+} )
 
 // update info for clients 
 
