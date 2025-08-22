16bf97b9adc619128080badc04c72a0193e55f43
root <187004982+ugbhbeh@users.noreply.github.com>
2025-08-22 05:37:38 +0000

    add bulk import route

--- routes/CustomerRouter.js
+++ routes/CustomerRouter.js
@@ -6,23 +6,42 @@ const prisma = new PrismaClient();
 
 // add csv of clients 
 
-CustomerRouter.post("/", authenticateToken, async(req, res) => {
-    const userId = req.user.userId
-    const customers = req.body
+CustomerRouter.post("/csv", authenticateToken, async (req, res) => {
+  const userId = req.user.userId;
+  const { clients } = req.body;
 
-    if (!Array.isArray(clients) || clients.length === 0) {
-     return res.status(400).json({ error: "No clients provided" });
-    };
+  if (!Array.isArray(clients) || clients.length === 0) {
+    return res.status(400).json({ error: "No clients provided" });
+  }
 
-    try{
+  try {
+    
+    const result = await prisma.customer.createMany({
+        data: clients.map((client) => ({
+        email: client.email,
+        name: client.name || null,
+        userId, 
+      })),
+      skipDuplicates: true, 
+    });
 
-    } 
+     const customers = await prisma.customer.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 
 
-}  {
-
-})
 // add manual clients 
 
 // update info for clients 
