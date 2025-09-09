9cdd1dd94f8a5abd383ab0d7f7a7463743788219
root <187004982+ugbhbeh@users.noreply.github.com>
2025-09-09 04:39:16 +0000

    added gpt api

---
+++ middleware/openai.js
@@ -0,0 +1,35 @@
+import ModelClient, { isUnexpected } from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+
+const token = process.env["GITHUB_TOKEN"];
+const endpoint = "https://models.github.ai/inference";
+const model = "openai/gpt-5-mini";
+
+export async function main() {
+
+  const client = ModelClient(
+    endpoint,
+    new AzureKeyCredential(token),
+  );
+
+  const response = await client.path("/chat/completions").post({
+    body: {
+      messages: [
+        { role:"system", content: "You are a helpful assistant." },
+        { role:"user", content: "What is the capital of France?" }
+      ],
+      model: model
+    }
+  });
+
+  if (isUnexpected(response)) {
+    throw response.body.error;
+  }
+
+  console.log(response.body.choices[0].message.content);
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
